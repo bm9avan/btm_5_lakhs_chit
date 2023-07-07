85018440fc1dad791dab691f834f2e2955d25698
bm9avan <mggpavan@gmail.com>
2023-07-07 20:15:53 +0530

    added key and formating

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const options = [
   {
     label: "5 Lakhs",
     value: "5",
-    month:"40"
+    month: "40"
   },
   {
     label: "2 Lakhs",
@@ -16,7 +16,7 @@ const options = [
   {
     label: "1 Lakhs",
     value: "1",
-    month:"10"
+    month: "10"
   },
 ];
 
@@ -24,23 +24,23 @@ export default function App() {
   const [dis, setDis] = useState("");
   const [chit, setChit] = useState(0)
   const inref = useRef();
-  useEffect(()=>{
+  useEffect(() => {
     inref.current.focus()
-  },[])
-  
-  function functionSet(v){
-    if(v==='5'){
+  }, [])
+
+  function functionSet(v) {
+    if (v === '5') {
       setChit(0)
-    }else if(v==='2'){
+    } else if (v === '2') {
       setChit(1)
-    }else {
+    } else {
       setChit(2)
     }
     setDis("");
     inref.current.value = "";
     inref.current.focus();
   }
-  
+
   function printHandler(e) {
     e.preventDefault();
     setDis(inref.current.value);
@@ -53,21 +53,21 @@ export default function App() {
     <div className="App">
       <h2>Manjunath - 9902781386</h2>
       <h4>
-        
-        {options[chit].month} Months <span><select value={options[chit].value} onChange={(e)=>functionSet(e.target.value)}>
-            {options.map((option) => (
-              <option value={option.value}>{option.label}</option>
-            ))}
-          </select></span>
+
+        {options[chit].month} Months <span><select value={options[chit].value} onChange={(e) => functionSet(e.target.value)}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>{option.label}</option>
+          ))}
+        </select></span>
       </h4>
       <form onSubmit={printHandler}>
         <label htmlFor="inMonth">Month: </label>
         <input ref={inref} type="number" min="1" max={options[chit].month} id="inMonth" placeholder="Enter Month"></input>
       </form>
       {dis !== "" ? (
-        <Table month={inref.current.value} chit={chit}/>
+        <Table month={inref.current.value} chit={chit} />
       ) : (
-          <p className="noData">"Choose a month <span>(1 to {options[chit].month})</span> for Chit selection"</p>
+        <p className="noData">"Choose a month <span>(1 to {options[chit].month})</span> for Chit selection"</p>
       )}
     </div>
   );
