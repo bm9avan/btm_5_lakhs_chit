cab6d80519206bb18187725e8036e6fed079f483
PAVAN BASAPATHI <mggpavan@gmail.com>
2023-07-11 13:22:41 +0000

    added new 5 Lakhs 20 Months

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Table from "./Table";
 
 const options = [
   {
-    label: "5 Lakhs",
-    value: "5",
+    label: "5 Lakhs(20 Months)",
+    value: "5-20",
+    month: "20"
+  },
+  {
+    label: "5 Lakhs(40 Months)",
+    value: "5-40",
     month: "40"
   },
   {
-    label: "2 Lakhs",
+    label: "2 Lakhs(20 Months)",
     value: "2",
     month: "20"
   },
   {
-    label: "1 Lakhs",
+    label: "1 Lakhs(20 Months)",
     value: "1",
     month: "20"
   },
@@ -29,12 +34,14 @@ export default function App() {
   }, [])
 
   function functionSet(v) {
-    if (v === '5') {
+    if (v === '5-20') {
       setChit(0)
-    } else if (v === '2') {
+    } else if (v === '5-40') {
       setChit(1)
-    } else {
+    } else if (v === '2') {
       setChit(2)
+    } else {
+      setChit(3)
     }
     setDis("");
     inref.current.value = "";
@@ -54,14 +61,13 @@ export default function App() {
       <h2>Manjunath - 9902781386</h2>
       <h4>
 
-        {options[chit].month} Months <span><select value={options[chit].value} onChange={(e) => functionSet(e.target.value)}>
+        <span><select value={options[chit].value} onChange={(e) => functionSet(e.target.value)}>
           {options.map((option) => (
             <option key={option.value} value={option.value}>{option.label}</option>
           ))}
         </select></span>
       </h4>
       <form onSubmit={printHandler}>
-        <label htmlFor="inMonth">Month: </label>
         <input ref={inref} type="number" min="1" max={options[chit].month} id="inMonth" placeholder="Enter Month"></input>
       </form>
       {dis !== "" ? (
--- src/Table.js
+++ src/Table.js
@@ -1,81 +1,98 @@
 import React, { useEffect, useState } from "react";
 
 export default function Table({ month, chit }) {
-    const [arr, setArr] = useState([]);
-    useEffect(() => {
-        let fakearr = [];
-        if (chit === 0) {
-            let amount = 472500;
-            for (let i = 0; i < 40; i++) {
-                if (i > 32) {
-                    amount += 5000;
-                } else {
-                    amount += 2500;
-                }
-                fakearr.push({ i: i + 1, amount });
-            }
+  const [arr, setArr] = useState([]);
+  useEffect(() => {
+    let fakearr = [];
+    if (chit === 0) {
+      let haraaju = 470000;
+      for (let i = 0; i < 20; i++) {
+        haraaju += 5000;
+        fakearr.push({ i: i + 1, haraaju });
+      }
+    } else if (chit === 1) {
+      let haraaju = 472500;
+      for (let i = 0; i < 40; i++) {
+        if (i > 32) {
+          haraaju += 5000;
         } else {
-            let haraaju = 24000;
-            let profit = 1000
-            for (let i = 0; i < 20; i++) {
-                haraaju -= 1000
-                profit -= 50
-                if (chit === 1) {
-                    fakearr.push({ i: i + 1, haraaju: haraaju * 2, profit: profit * 2, emi: (5000 - profit) * 2 });
-                } else {
-                    fakearr.push({ i: i + 1, haraaju, profit, emi: 5000 - profit });
-                }
-            }
+          haraaju += 2500;
         }
-        setArr(fakearr);
-    }, [month, chit]);
-    return (
-        <>
-            <h3>
-                You will recive amount {arr[+month - 1] ? arr[+month - 1].amount : ''} in {month}th month
-            </h3>
-            {chit === 0 && <table className="result">
-                <thead>
-                    <tr>
-                        <th>Month</th>
-                        <th>EMI</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {arr.map((each) => {
-                        return (
-                            <tr key={each.i} id={each.i === +month ? "highlight" : ""}>
-                                <td>{each.i} </td>
-                                <td>12,500</td>
-                                <td>{each.amount}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>}
-            {chit !== 0 && <table className="result">
-                <thead>
-                    <tr>
-                        <th>Month</th>
-                        <th>Haraaju</th>
-                        <th>Profit</th>
-                        <th>EMI</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {arr.map((each) => {
-                        return (
-                            <tr key={each.i} id={each.i === +month ? "highlight" : ""}>
-                                <td>{each.i} </td>
-                                <td>{each.haraaju}</td>
-                                <td>{each.profit}</td>
-                                <td>{each.emi}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>}
-        </>
-    );
+        fakearr.push({ i: i + 1, haraaju });
+      }
+    } else {
+      let haraaju = 24000;
+      let profit = 1000;
+      for (let i = 0; i < 20; i++) {
+        haraaju -= 1000;
+        profit -= 50;
+        if (chit === 2) {
+          //2 Lakhs
+          fakearr.push({
+            i: i + 1,
+            haraaju: haraaju * 2,
+            profit: profit * 2,
+            emi: (5000 - profit) * 2,
+          });
+        } else {
+          fakearr.push({ i: i + 1, haraaju, profit, emi: 5000 - profit });
+        }
+      }
+    }
+    setArr(fakearr);
+  }, [month, chit]);
+  return (
+    <>
+      <h3>
+        You will recive amount {arr[+month - 1] ? arr[+month - 1].haraaju : ""}{" "}
+        in {month}th month
+      </h3>
+      {(chit === 0 || chit === 1) && (
+        <table className="result">
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>EMI</th>
+              <th>Haraaju</th>
+            </tr>
+          </thead>
+          <tbody>
+            {arr.map((each) => {
+              return (
+                <tr key={each.i} id={each.i === +month ? "highlight" : ""}>
+                  <td>{each.i} </td>
+                  <td>{chit === 0 ? "25,000" : "12,500"}</td>
+                  <td>{each.haraaju}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+      {chit !== 0 && chit !== 1 && (
+        <table className="result">
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>Haraaju</th>
+              <th>Profit</th>
+              <th>EMI</th>
+            </tr>
+          </thead>
+          <tbody>
+            {arr.map((each) => {
+              return (
+                <tr key={each.i} id={each.i === +month ? "highlight" : ""}>
+                  <td>{each.i} </td>
+                  <td>{each.haraaju}</td>
+                  <td>{each.profit}</td>
+                  <td>{each.emi}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
